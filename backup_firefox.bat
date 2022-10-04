b4dc490b79a2f13567e732bcc65372c15118b84a
Rodolfo Souza <83579016+rodhfr@users.noreply.github.com>
2022-10-04 05:07:33 +0000

    Update backup_firefox.bat

--- backup_firefox.bat
+++ backup_firefox.bat
@@ -4,9 +4,9 @@ set /p "reply=Time of the week to do the Firefox  semanal backup, you need to cl
 if /i not "%reply%" == "y" goto :eof
 echo Closing Firefox...
 taskkill /IM firefox.exe >nul
-cd C:\Users\souza\AppData\Roaming\Mozilla\Firefox\Profiles
+cd C:\Users\%username%\AppData\Roaming\Mozilla\Firefox\Profiles
 echo Zipping Files to upload...
 ping -n 20 127.0.0.1>nul
-7z u -t7z -r C:\Users\souza\Documents\FirefoxBackup\firefox_backup profile 
-rclone sync C:\Users\souza\Documents\FirefoxBackup cryptsouzafrodolfo:Firefox --verbose
+7z u -t7z -r C:\Users\%username%\Documents\FirefoxBackup\firefox_backup profile 
+rclone sync C:\Users\%username%\Documents\FirefoxBackup cryptremote:Firefox --verbose
 :EOF
